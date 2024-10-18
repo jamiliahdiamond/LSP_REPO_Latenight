1d712696b20ab222b754dc83be7ad84b58879415
JamiliahDiamond <jamiliaheubanks@gmail.com>
2024-10-17 20:56:24 -0400

    2nd test run

--- LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question1/Book.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question1/Book.java
@@ -1,10 +1,64 @@
 package org.howard.edu.lsp.midterm.question1;
 
 public class Book {
+	// book details that are private so no one edits
+    private String title;
+    private String author;
+    private String isbn;
+    private int yearPublished;
+// creating a new book object
+    public Book(String title, String author, String isbn, int yearPublished) {
+        // book details for when it is created
+    	this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.yearPublished = yearPublished;
+    }
+    // ability to get the book details
+    public String getTitle() {
+        return title;
+    }
+    // ability to change book details
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public String getAuthor() {
+        return author;
+    }
 
-	}
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+    // check if two books are the same
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Book book = (Book) obj;
+        return isbn.equals(book.isbn) && author.equals(book.author);
+    }
+    // string description of the book
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", ISBN: " + isbn + ", Year Published: " + yearPublished;
+    }
 }
+
+
--- LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question1/BookDriver.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question1/BookDriver.java
@@ -1,10 +1,17 @@
 package org.howard.edu.lsp.midterm.question1;
 
 public class BookDriver {
+    public static void main(String[] args) {
+    	// created three book objects to make test cases
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "123456789", 1925);
+        Book book2 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "123456789", 1925);
+        Book book3 = new Book("A Promised Land", "Barack Obama", "9780593239681", 2020);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        // should print out true 
+        System.out.println(book1.equals(book2));
+        // should print out false
+        System.out.println(book1.equals(book3));
+        // should print out book info
+        System.out.println(book1); 
+    }
 }
