ffe1b264478380bfcf342d942e500fa4aa8f5c32
JamiliahDiamond <jamiliaheubanks@gmail.com>
2024-12-05 20:32:10 -0500

    question 3

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/finalexam/question3/Rectangle.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.finalexam.question3;
+
+//Rectangle shape
+public class Rectangle implements Shape {
+ // Draw method for Rectangle
+ @Override
+ public void draw() {
+     System.out.println("Drawing a Rectangle");
+ }
+}
