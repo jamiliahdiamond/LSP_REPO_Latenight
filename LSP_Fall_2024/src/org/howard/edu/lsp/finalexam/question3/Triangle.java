c744fdc45250620614c5a23d87e575ac961d68e8
JamiliahDiamond <jamiliaheubanks@gmail.com>
2024-12-05 20:32:46 -0500

    Question 3

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/finalexam/question3/Triangle.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.finalexam.question3;
+
+//Triangle shape
+public class Triangle implements Shape {
+ // Draw method for Triangle
+ @Override
+ public void draw() {
+     System.out.println("Drawing a Triangle");
+ }
+}
