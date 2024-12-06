0dfdde794d55725fea17355baaa8bf1f04550b6e
JamiliahDiamond <jamiliaheubanks@gmail.com>
2024-12-05 20:31:25 -0500

    question 3 for final

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/finalexam/question3/Circle.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.finalexam.question3;
+
+//Circle shape
+public class Circle implements Shape {
+ // Draw method for Circle
+ @Override
+ public void draw() {
+     System.out.println("Drawing a Circle");
+ }
+}
